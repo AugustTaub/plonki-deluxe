af966f5f10ae16e1f73acf4577ddb6069dd6b4e9
August <47953647+AugustTaub@users.noreply.github.com>
2026-04-21 17:23:09 +0200

    level system start

---
+++ scripts/stage_manager.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+@export var stage_nodes: Array[StageNode]
+var stage_i: int = 0
+var curr_stage_node: StageNode
+
+var away_point: Vector2 = Vector2(0,5000)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_stage_node(stage_nodes[0])
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		stage_i += 1
+		if stage_i > stage_nodes.size()-1:
+			stage_i = 0
+		
+		set_stage_node(stage_nodes[stage_i])
+
+func set_stage_node(new_stage_node: StageNode):
+	
+	new_stage_node.global_position = global_position
+	
+	if curr_stage_node:
+		curr_stage_node.global_position = away_point
+	
+	curr_stage_node = new_stage_node
---
+++ scripts/stage_node.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+class_name StageNode
+
+@export var stage_id: int = -1
+@export var is_unlocked: bool = true
+@export var stage_name: String = "ERROR - FALLBACK"
+ 
